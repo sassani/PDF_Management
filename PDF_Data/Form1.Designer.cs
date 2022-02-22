0f93117127fcfff306fe4ab5cf93dee940e16602
Ardavan Sassani <a.sassani@gmail.com>
2022-02-22 18:16:17 -0500

    manual rect

--- PDF_Data/Form1.Designer.cs
+++ PDF_Data/Form1.Designer.cs
@@ -30,6 +30,18 @@ namespace PDF_Data
         private void InitializeComponent()
         {
             this.btnOpenPdf = new System.Windows.Forms.Button();
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtW = new System.Windows.Forms.TextBox();
+            this.txtH = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPreview = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnOpenPdf
@@ -42,21 +54,146 @@ namespace PDF_Data
             this.btnOpenPdf.UseVisualStyleBackColor = true;
             this.btnOpenPdf.Click += new System.EventHandler(this.btnOpenPdf_Click);
             // 
+            // wb
+            // 
+            this.wb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wb.Location = new System.Drawing.Point(12, 237);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Name = "wb";
+            this.wb.Size = new System.Drawing.Size(960, 568);
+            this.wb.TabIndex = 1;
+            this.wb.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wbReader_DocumentCompleted);
+            this.wb.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.wbReader_Navigating);
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(12, 34);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(56, 20);
+            this.txtX.TabIndex = 1;
+            this.txtX.Text = "0";
+            this.txtX.TextChanged += new System.EventHandler(this.txtX_TextChanged);
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(74, 34);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(56, 20);
+            this.txtY.TabIndex = 2;
+            this.txtY.Text = "0";
+            this.txtY.TextChanged += new System.EventHandler(this.txtX_TextChanged);
+            // 
+            // txtW
+            // 
+            this.txtW.Location = new System.Drawing.Point(136, 34);
+            this.txtW.Name = "txtW";
+            this.txtW.Size = new System.Drawing.Size(56, 20);
+            this.txtW.TabIndex = 3;
+            this.txtW.Text = "200";
+            this.txtW.TextChanged += new System.EventHandler(this.txtX_TextChanged);
+            // 
+            // txtH
+            // 
+            this.txtH.Location = new System.Drawing.Point(198, 34);
+            this.txtH.Name = "txtH";
+            this.txtH.Size = new System.Drawing.Size(56, 20);
+            this.txtH.TabIndex = 4;
+            this.txtH.Text = "200";
+            this.txtH.TextChanged += new System.EventHandler(this.txtX_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtH);
+            this.panel1.Controls.Add(this.txtX);
+            this.panel1.Controls.Add(this.txtW);
+            this.panel1.Controls.Add(this.txtY);
+            this.panel1.Location = new System.Drawing.Point(93, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(265, 64);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "W:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(195, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "H:";
+            // 
+            // txtPreview
+            // 
+            this.txtPreview.Location = new System.Drawing.Point(39, 146);
+            this.txtPreview.Multiline = true;
+            this.txtPreview.Name = "txtPreview";
+            this.txtPreview.Size = new System.Drawing.Size(863, 637);
+            this.txtPreview.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(984, 817);
+            this.Controls.Add(this.txtPreview);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.wb);
             this.Controls.Add(this.btnOpenPdf);
             this.Name = "Form1";
             this.Text = "PDF Management";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnOpenPdf;
+        private System.Windows.Forms.WebBrowser wb;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtW;
+        private System.Windows.Forms.TextBox txtH;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPreview;
     }
 }
 
--- PDF_Data/Form1.cs
+++ PDF_Data/Form1.cs
@@ -7,6 +7,8 @@ using iText.Kernel.Geom;
 using iText.Kernel.Pdf.Canvas.Parser.Filter;
 using System.Text;
 
+using IronPdf;
+
 namespace PDF_Data
 {
     public partial class Form1 : Form
@@ -18,27 +20,80 @@ namespace PDF_Data
 
         private void btnOpenPdf_Click(object sender, EventArgs e)
         {
+            //string dest = "https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.webbrowser.document?view=netframework-4.7.2";
             string dest = "C:\\Users\\asassani1\\OneDrive - Georgia State University\\DICE\\DengAI\\data\\DataCollection\\peru\\2020\\23.pdf";
-            PdfDocument pdfDoc = new PdfDocument(new PdfReader(dest));
+            //iText.Kernel.Pdf.PdfDocument pdfDoc = new iText.Kernel.Pdf.PdfDocument(new PdfReader(dest));
+            //IronPdf.PdfDocument ironPdf = IronPdf.PdfDocument.FromFile(dest);
 
-            Rectangle rect = new Rectangle(100, 100, 200, 200);
+            //string AllText = ironPdf.ExtractAllText();
+            wb.Url = new Uri(dest);
+            //HtmlDocument test = wbReader.Document;
+
+            Rectangle rect = new Rectangle(0, 0, 250, 200);
             getDataFromRect(rect);
 
         }
 
+        bool WebBrowserDocumentEventSet = false;
+
+        private iText.Kernel.Pdf.PdfDocument pdfDoc;
+
+
         private void getDataFromRect(Rectangle rect)
         {
             string dest = "C:\\Users\\asassani1\\OneDrive - Georgia State University\\DICE\\DengAI\\data\\DataCollection\\peru\\2020\\23.pdf";
-            PdfDocument pdfDoc = new PdfDocument(new PdfReader(dest));
-            TextRegionEventFilter regionFilter = new TextRegionEventFilter(rect);
-            StringBuilder sb = new StringBuilder();
-            for (int page = 1; page <= pdfDoc.GetNumberOfPages(); page++)
-            {
-                ITextExtractionStrategy strategy = new FilteredTextEventListener(new LocationTextExtractionStrategy(), regionFilter);
-                String str = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(page), strategy) + "\n\n";
-                sb.Append(str);
-            }
+            pdfDoc = new iText.Kernel.Pdf.PdfDocument(new PdfReader(dest));
+            //TextRegionEventFilter regionFilter = new TextRegionEventFilter(rect);
+            //StringBuilder sb = new StringBuilder();
+            //for (int page = 1; page <= pdfDoc.GetNumberOfPages(); page++)
+            //{
+            //    ITextExtractionStrategy strategy = new FilteredTextEventListener(new LocationTextExtractionStrategy(), regionFilter);
+            //    String str = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(page), strategy) + "\n\n";
+            //    sb.Append(str);
+            //}
 
+        }
+
+        private void GetPreviewDataFromRect()
+        {
+            Rectangle rect = new Rectangle(
+               float.Parse(txtX.Text),
+                float.Parse(txtY.Text),
+                float.Parse(txtW.Text),
+                float.Parse(txtH.Text)
+                );
+            TextRegionEventFilter regionFilter = new TextRegionEventFilter(rect);
+            ITextExtractionStrategy strategy = new FilteredTextEventListener(new LocationTextExtractionStrategy(), regionFilter);
+            String str = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(1), strategy);
+            txtPreview.Text = str;
+        }
+
+        private void wbReader_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //WebBrowser wb = (sender as WebBrowser);
+            if (wb.ReadyState == WebBrowserReadyState.Complete && WebBrowserDocumentEventSet == false)
+            {
+                //WebBrowserDocumentEventSet = true;
+                //wb.Document.MouseDown += OnHtmlDocumentClick;
+            }
+            Console.WriteLine(wb.DocumentType);
+            //wb.DocumentType.MouseDown += OnHtmlDocumentClick;
+        }
+
+        private void wbReader_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            //(sender as WebBrowser).Document.MouseDown -= OnHtmlDocumentClick;
+            //WebBrowserDocumentEventSet = false;
+        }
+
+        private void OnHtmlDocumentClick(object sender, HtmlElementEventArgs e)
+        {
+            Console.WriteLine(e.ClientMousePosition);
+        }
+
+        private void txtX_TextChanged(object sender, EventArgs e)
+        {
+            GetPreviewDataFromRect();
         }
     }
 }
