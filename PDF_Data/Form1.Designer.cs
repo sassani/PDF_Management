871dba80909388137ba8c58621ae70f88d045e9d
Ardavan Sassani <a.sassani@gmail.com>
2022-02-22 08:59:04 -0500

    Add project files.

---
+++ PDF_Data/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace PDF_Data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenPdf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenPdf
+            // 
+            this.btnOpenPdf.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenPdf.Name = "btnOpenPdf";
+            this.btnOpenPdf.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenPdf.TabIndex = 0;
+            this.btnOpenPdf.Text = "button1";
+            this.btnOpenPdf.UseVisualStyleBackColor = true;
+            this.btnOpenPdf.Click += new System.EventHandler(this.btnOpenPdf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenPdf);
+            this.Name = "Form1";
+            this.Text = "PDF Management";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenPdf;
+    }
+}
+
---
+++ PDF_Data/Form1.cs
@@ -0,0 +1,32 @@
+﻿using iText.Kernel.Pdf;
+using System;
+using System.Windows.Forms;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Pdf.Canvas.Parser;
+
+namespace PDF_Data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenPdf_Click(object sender, EventArgs e)
+        {
+            string dest = "C:\\Users\\Ardavan\\OneDrive - Georgia State University\\DICE\\DengAI\\data\\DataCollection\\peru\\2020\\23.pdf";
+            PdfDocument pdfDoc = new PdfDocument(new PdfReader(dest));
+            ITextExtractionStrategy extractionStrategy = new SimpleTextExtractionStrategy();
+            //IPdfTextLocation textLocation = new DefaultPdfTextLocation(1, new iText.Kernel.Geom.Rectangle(20, 2), "");
+            for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+            {
+                IPdfTextLocation textLocation = new DefaultPdfTextLocation(i, new iText.Kernel.Geom.Rectangle(20, 2), "");
+                //string sPageText = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(i), extractionStrategy);
+                string sPageText ="";
+                Console.WriteLine(sPageText);
+            }
+
+        }
+    }
+}
